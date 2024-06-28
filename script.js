8546bb3d4c558535cd18e823e756b28435eac12e
peptang12 <nasiumabrav@gmail.com>
2024-06-28 13:33:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let person = {
+    name : " bravin",
+    age : 29,
+    id : 12422
+}
+ person.name = " emmanuel";
+console.log(person);
+function greet(name)
+{
+    console.log("Hello " + name);
+}
+greet(" bravin");
+function square(number){
+
+return number * number; 
+
+}
+let number = square(7);
+console.log(number);
+console.log(square(3));
